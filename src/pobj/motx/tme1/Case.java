b8318c39a88e91a2e40fe807843e7bc1ed559757
3520014 <3520014@ppti-14-301-01.ufr-info-p6.jussieu.fr>
2019-01-30 12:00:32 +0100

    commit fin de séance

--- src/pobj/motx/tme1/Case.java
+++ src/pobj/motx/tme1/Case.java
@@ -1,14 +1,45 @@
 package pobj.motx.tme1;
 
 public class Case {
-	private int colonne, ligne;
+	private final int colonne, ligne;
 	private char valeur;
 	
 	public Case(int lig,int col,char c) {
 		this.ligne=lig;
 		this.colonne=col;
-		
+		this.valeur=c;
 	}
+	
+	
+	public int getLig() { return ligne; }
+	public int getCol() { return colonne; }
+	public char getChar() { return valeur; }
+	
+	public void setChar(char c) {
+		this.valeur=c;
+	}
+	
+	public boolean isVide() {
+		if(valeur== ' ') {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean isPleine() {
+		if(valeur =='*') {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	
+	
+	
 	
 
 }
--- src/pobj/motx/tme1/Grille.java
+++ src/pobj/motx/tme1/Grille.java
@@ -1,5 +1,10 @@
 package pobj.motx.tme1;
 
 public class Grille {
+	private Case[][] case;
+	
+	public Grille(int hauteur, int largeur) {
+		
+	}
 
 }
