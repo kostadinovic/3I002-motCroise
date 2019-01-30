38325f5412576138ec2cf71d6a51a2e0cf219c00
3520014 <3520014@ppti-14-301-01.ufr-info-p6.jussieu.fr>
2019-01-30 11:33:37 +0100

    1er test commit

---
+++ src/pobj/motx/tme1/Case.java
@@ -0,0 +1,14 @@
+package pobj.motx.tme1;
+
+public class Case {
+	private int colonne, ligne;
+	private char valeur;
+	
+	public Case(int lig,int col,char c) {
+		this.ligne=lig;
+		this.colonne=col;
+		
+	}
+	
+
+}
