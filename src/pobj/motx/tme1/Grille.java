c049b0328b21fe6c12c9de06594e66b88345bff8
Nemanja <nemanjakostadinovic96@gmail.com>
2019-01-31 19:42:57 +0100

    classe Grille fini et tester ok

--- src/pobj/motx/tme1/Grille.java
+++ src/pobj/motx/tme1/Grille.java
@@ -1,10 +1,41 @@
 package pobj.motx.tme1;
 
 public class Grille {
-	private Case[][] case;
+	private Case[][] matrice;
 	
 	public Grille(int hauteur, int largeur) {
-		
+		matrice= new Case[hauteur][largeur];
+		for(int i=0;i<hauteur;i++) {
+			for(int j=0;j<largeur;j++) {
+				this.matrice[i][j] = new Case(i,j,' ');		
+			}
+		}
 	}
-
+	
+	public Case getCase(int lig,int col){
+		return matrice[lig][col];	
+	}
+	
+	@Override
+	public String toString() {
+		return GrilleLoader.serialize(this, false);
+	}
+	
+	public int nbLig() {
+		return matrice.length;
+	}
+	
+	public int nbCol() {
+		return matrice[0].length;
+	}
+	
+	public Grille copy() {
+		Grille copy = new Grille(this.nbLig(),this.nbCol());
+		for (int i=0; i<this.nbLig();i++) {
+			for (int j=0; j<nbCol();j++) {
+					copy.getCase(i, j).setChar(getCase(i, j).getChar());		
+			}
+		}
+		return copy;
+	}
 }
